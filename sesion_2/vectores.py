8759278eb4736df18bfaa74783959fdd18ac965f
kmmx <kmmx@hsoft.local>
2016-08-27 16:07:23 -0500

    Sesión 2 - vectores.py actualizado

--- sesion_2/vectores.py
+++ sesion_2/vectores.py
@@ -48,8 +48,17 @@ def times_vec(a, b):
 def norm_vec(u):
     return dot_vec(u, u) ** 0.5
 
-def unit(u):
+def unit_vec(u):
     return mul_vec(1.0 / norm_vec(u), u)
 
 def proj_vec(a, b):
-    return mul_vec(norm_vec(a), unit(b)) 
+    return mul_vec(norm_vec(a), unit_vec(b))
+
+print add_vec((1, 2, 3), (4, 5, 6))
+print sub_vec((1, 2, 3), (4, 5, 6))
+print mul_vec(2, (4, 5, 6))
+print dot_vec((1, 2, 3), (4, 5, 6))
+print times_vec((1, 2, 3), (4, 5, 6))
+print norm_vec((1, 2, 3))
+print unit_vec((1, 2, 3))
+print proj_vec((1, 2, 3), (4, 5, 6))
