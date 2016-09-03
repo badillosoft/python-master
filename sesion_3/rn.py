62ac82af4e33d15134943528e7b7075452bd605d
kmmx <kmmx@hsoft.local>
2016-09-03 18:38:52 -0500

    rn

--- sesion_3/rn.py
+++ sesion_3/rn.py
@@ -1,19 +1,19 @@
-from random import rand
+from random import random
 from math import *
 
 class Node:
     def __init__(self, n):
         self.n = n
         self.w = []
-        self.w0 = rand()
+        self.w0 = random()
 
         for i in range(0, n):
-            self.w.append(rand())
+            self.w.append(random())
 
     def eval(self, x):
         s = self.w0
         for i in range(0, self.n):
-            s += self.w[i] * self.x[i]
+            s += self.w[i] * x[i]
 
         return 1.0 / (1.0 + exp(-s))
 
@@ -38,19 +38,26 @@ class Layer:
 class RN:
     def __init__(self, M, N, K, G):
         self.layers = [Layer(K, N)]
+        self.M = M
 
         for i in range(0, M):
-            self.layers(Layer(N, N))
+            self.layers.append(Layer(N, N))
 
-        self.layers(Layer(N, G))
+        self.layers.append(Layer(N, G))
 
     def eval(self, x):
         y = self.layers[0].eval(x)
 
-        for i in range(1, M + 1):
+        for i in range(1, self.M + 1):
             y = self.layers[1].eval(y)
 
-        z = self.layers[M + 1].eval(y)
+        z = self.layers[self.M + 1].eval(y)
 
         return z
 
+rn = RN(1, 4, 2, 1)
+
+print rn.eval([0, 0])
+print rn.eval([0, 1])
+print rn.eval([1, 0])
+print rn.eval([1, 1])
